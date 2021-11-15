69980f0efc18e093409128efe31d4725b7b63d6b
RumenKanev <rkynev@gmail.com>
2021-11-15 15:33:16 +0000

    initial commit

---
+++ Price.java
@@ -0,0 +1,8 @@
+class PriceDisplay {
+    public static void main(String[] args) {
+        String item = "Coffee machine";
+        Double price = 15.99;
+        System.out.printf("The price of the item %s is %.2f \n", item, price);
+    }
+}
+
